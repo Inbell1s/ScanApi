cc64629e215b081e39403289686ba18d79f34d23
Inbell1s <50827900+Inbell1s@users.noreply.github.com>
2020-06-27 18:03:19 +0200

    Added working linkt to Amass latest version.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/
+!/bin/bash/
 apt-get update -y;
 apt-get install -y python3 python3-pip python3-dev build-essential golang wget unzip git;
 pip3 install -r requirements.txt;
@@ -9,9 +9,10 @@ go get -u github.com/OJ/gobuster;
 go get -u github.com/tomnomnom/assetfinder;
 go get -u github.com/tomnomnom/httprobe;
 go get -u -v github.com/projectdiscovery/subfinder/cmd/subfinder;
-wget https://github.com/OWASP/Amass/releases/download/v3.1.10/amass_v3.1.10_linux_amd64.zip;
+wget https://github.com/OWASP/Amass/releases/download/v3.7.2/amass_linux_amd64.zip;
 wget https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux;
 chmod +x findomain-linux;
-unzip amass_v3.1.10_linux_amd64.zip;
+unzip amass_linux_amd64.zip;
 cp findomain-linux $GOPATH/bin/findomain;
-cp amass_v3.1.10_linux_amd64/amass $GOPATH/bin/;
+cp amass_linux_amd64/amass $GOPATH/bin/;
+
